188b41de835d880c048dfe6c69506e2c0bd9830d
San-tiago <62841279+San-tiago@users.noreply.github.com>
2020-10-14 21:37:52 +0800

    Sopienzolfe

---
+++ Sopien/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Socialite;
+use App\User;
+use Auth;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectToProvider()
+    {
+        return Socialite::driver('google')->stateless()->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {   // add lang pala yung stateless chuchu inamyan
+        $user = Socialite::driver('google')->stateless()->user();
+        
+        $user = User::firstorCreate([
+            'name' => $user->getName(),
+            'email' => $user->getEmail(),
+            'provider_id' => $user->getId()
+
+        ]);
+        // $user->token;
+
+        Auth::Login($user,true);
+        return redirect('/home');
+    }
+
+}
